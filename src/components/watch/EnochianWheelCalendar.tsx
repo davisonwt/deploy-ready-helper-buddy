3cb44d7f698646fdf1518ad480089814fa797a73
davison <davison.taljaard@icloud.com>
2025-11-30 08:09:10 +0200

    Fix wheel visibility: Position wheel to left side while keeping it visible

--- src/components/watch/EnochianWheelCalendar.tsx
+++ src/components/watch/EnochianWheelCalendar.tsx
@@ -4061,7 +4061,7 @@ const EnochianTimepiece = () => {
       {/* Main Content Container */}
       <div className="relative z-10 flex flex-col lg:flex-row items-center justify-between gap-8" style={{ padding: '5cm 5cm 5cm 5cm', minHeight: 'calc(100vh - 10cm)' }}>
         
-        {/* Calendar Wheel - Left Side - MUCH BIGGER - off-center to the left */}
+        {/* Calendar Wheel - Left Side - MUCH BIGGER - positioned to the left */}
         <motion.div 
           initial={{ scale: 0.8, opacity: 0 }} 
           animate={{ scale: 1, opacity: 1 }} 
@@ -4073,7 +4073,8 @@ const EnochianTimepiece = () => {
             minWidth: '50cm',
             minHeight: '50cm',
             maxWidth: 'calc(100% - 350px)',
-            paddingLeft: '2cm'
+            marginLeft: '0',
+            marginRight: 'auto'
           }}
         >
           <svg width="100%" height="100%" viewBox={`0 0 ${size} ${size}`} className="w-full h-full" preserveAspectRatio="xMidYMid meet">
